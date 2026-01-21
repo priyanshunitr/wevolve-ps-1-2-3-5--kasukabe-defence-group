117e0feec4fd180335b06566f10766a81ecdb9d1
priyanshunitr <sahupriyanshu2006@gmail.com>
2026-01-21 11:56:39 +0530

    spline tag remove

--- frontend/src/components/3d/splinebg.tsx
+++ frontend/src/components/3d/splinebg.tsx
@@ -3,8 +3,9 @@ import Spline from "@splinetool/react-spline";
 
 export default function SplineBg() {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full relative">
       <Spline scene="https://prod.spline.design/eptFcGbRN8-SmKyB/scene.splinecode" />
+      <div className="absolute bottom-0 right-0 w-48 h-16 bg-black" />
     </div>
   );
 }
